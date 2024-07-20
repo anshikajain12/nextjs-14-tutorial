3e9c540fbcfa53b89d5f210f780a1b67471fb4a4
anshika <nisthajain2412@gmail.com>
2024-07-20 08:00:47 +0530

    static rendering of client component

---
+++ rendering-demo/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react"
+
+export default function DashboardPage() {
+    console.log("Dashboard client component")
+    const [name, setName] = useState("");
+    return (
+        <>
+            <h1>Dashboard</h1>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <p>Hello, {name}</p>
+        </>
+    )
+}
