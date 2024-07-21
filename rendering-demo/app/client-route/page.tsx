9f602a05d255a94e263c582fedaa91f6ca8a7694
anshika <nisthajain2412@gmail.com>
2024-07-21 21:45:47 +0530

    server-only implementation

--- rendering-demo/app/client-route/page.tsx
+++ rendering-demo/app/client-route/page.tsx
@@ -4,11 +4,11 @@ import { serverSideFunction } from "@/utils/server-utils";
 
 export default function ClientRoutePage(){
     console.log("client Route render")
-    const result = serverSideFunction();
+    // const result = serverSideFunction();
     return (
         <>
             <h1>Client Route Page</h1>
-            <p>{result}</p>
+            {/* <p>{result}</p> */}
         </>
     )
 }
